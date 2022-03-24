2a7b115d5e5f69eb53490c30696fe0abee4089b5
mizxinp <mizxinp@gmail.com>
2022-03-24 23:59:34 +0800

    update

--- src/leetcode/linkedList/19/removeNthFromEnd.js
+++ src/leetcode/linkedList/19/removeNthFromEnd.js
@@ -1,19 +1,18 @@
 function removeNthFromEnd(head, n) {
+  if (!head) return null;
 
-    if (!head) return null
+  let dummy = new ListNode(null, head);
+  let [slowPoint, fasterPoint] = [dummy, dummy];
 
-    let dummy = new ListNode(null, head)
-    let [slowPoint, fasterPoint] = [dummy, dummy]
+  while (n !== 0) {
+    fasterPoint = fasterPoint.next;
+    n--;
+  }
 
-    while (n !== 0) {
-        fasterPoint = fasterPoint.next
-        n--
-    }
-
-    while (fasterPoint.next) {
-        slowPoint = slowPoint.next
-        fasterPoint = fasterPoint.next
-    }
-    slowPoint.next = slowPoint.next.next
-    return dummy.next
+  while (fasterPoint.next) {
+    slowPoint = slowPoint.next;
+    fasterPoint = fasterPoint.next;
+  }
+  slowPoint.next = slowPoint.next.next;
+  return dummy.next;
 }
